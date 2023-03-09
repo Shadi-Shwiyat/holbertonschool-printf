b94baa570226eda14a97abbcaa74968465824367
Tribeoftech <gratefulgrowing333@gmail.com>
2023-03-09 13:51:43 -0800

    printf

--- main.h
+++ main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+
+
 int _printf(const char *format, ...);
 int _putchar(char c);
-
+int func_char(va_list list);
+int func_string(va_list list);
+int func_percent(__attribute__((unused)) va_list list);
+int func_digit(va_list list);
 #endif

434bd9c87833f4916b7f2d66cb96f58d2b88625d
Shadi-Shwiyat <raz611103@gmail.com>
2023-03-09 09:39:23 -0800

    Function that produces output according to a format like printf

--- _printf.c
+++ _printf.c
@@ -18,11 +18,17 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	for (i = 0; format && format[i] != '\0'; i++)
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
+		if (format[i] == '%')
 		{
 			i++;
+			if (format[i] == '\0')
+				return (-1);
+
 			if (format[i] == 'c')
 			{
 				char c = va_arg(args, int);
@@ -51,7 +57,7 @@ int _printf(const char *format, ...)
 				len++;
 				i++;
 			}
-			else if (format[i] != 'c' || format[i] != 's' || format[i] != '%')
+			else
 				return (-1);
 		}
 		_putchar(format[i]);

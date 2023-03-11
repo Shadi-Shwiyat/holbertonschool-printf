058c2ccfb7e882061804831232f16585263d5536
Tribeoftech <gratefulgrowing333@gmail.com>
2023-03-10 17:14:37 -0800

    printfix

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
- /**
+
+/**
   * get_func - pointer to array
   * @conv_spec: specifiers
   * Return: function that is pointed to
@@ -25,61 +26,62 @@ int (*get_func(char conv_spec))(va_list)
 	return (funct[j].func);
 }
 
-
-/** _printf - Function produces output according to
- * a format
+/**
+ * _printf - Prints formatted output
  *
- * @format: the specified format to print the output
+ * @format: String containing format specifiers
  *
- * Return: the number of chars printed - null byte,
- * otherwise NULL
+ * Return: Length of the printed string
  */
-
 int _printf(const char *format, ...)
 {
 	int i, len;
 	va_list args;
-	
+
 	va_start(args, format);
 
 	i = 0;
 	len = 0;
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%' && format[i+1] != '\0')
-		{
-			i++;
-			if (format[i] == 'c')
-			{
-				char c = va_arg(args, int);
-				_putchar(c);
-				len++;
-			}
-			else if (format[i] == 's')
-			{
-				char *s = va_arg(args, char *);
-				while (*s != '\0')
-				{
-					_putchar(*s);
-					s++;
-					len++;
-				}
-			}
-			else if (format[i] == '%')
-			{
-				_putchar('%');
-				len++;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			len++;
-		}
-		i++;
+	if (format[i] == '%' && format[i + 1] != '\0')
+	{
+	i++;
+	if (format[i] == 'c')
+	{
+	char c = va_arg(args, int);
+	putchar(c);
+	len++;
 	}
-
+	else if (format[i] == 's')
+	{
+	char *s = va_arg(args, char *);
+	while (*s != '\0')
+	{
+	putchar(*s);
+	s++;
+	len++;
+	}
+	}
+	else if (format[i] == '%')
+	{
+	putchar('%');
+	len++;
+	}
+	else
+	{
+	putchar(format[i]);
+	len++;
+	}
+	}
+	else
+	{
+	putchar(format[i]);
+	len++;
+	}
+	i++;
+	}
 	va_end(args);
-
 	return (len);
 }
+
